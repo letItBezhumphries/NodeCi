bd7d6a22df55c9ab7133bbfbf5bf03dc1ef0ea3a
Eric Humphries <32630651+letItBezhumphries@users.noreply.github.com>
2023-02-01 13:31:52 -0800

    Update userFactory.js

--- tests/factories/userFactory.js
+++ tests/factories/userFactory.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 const User = mongoose.model("User");
 
-// module.exports = async () => {
-//   const user = await new User({}).save();
-//   return user;
-// };
-
 module.exports = () => {
   return new User({}).save();
 };
